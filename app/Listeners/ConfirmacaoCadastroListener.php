88ace7de56c9b9a85503c2b8cc68044ed5fae8ab
Mateus Carvalho <mateus@desktop.local>
2021-01-17 22:30:05 -0300

    Add queue

--- app/Listeners/ConfirmacaoCadastroListener.php
+++ app/Listeners/ConfirmacaoCadastroListener.php
@@ -8,8 +8,10 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class ConfirmacaoCadastroListener
+class ConfirmacaoCadastroListener implements ShouldQueue
 {
+
+    public $tries = 5;
 
     /**
      * Handle the event.
