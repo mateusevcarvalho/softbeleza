8d0c9b1c1ec3c6e76a592c77099d250ab571c42f
Mateus Carvalho <mateus@desktop.local>
2021-01-18 17:29:17 -0300

    Recuperacao de senha

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="{{url('images/softbeleza-logo-azul.png')}}" alt="Softbeleza Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
